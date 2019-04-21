3de41758c74afb1b8add6fb8bcede61a9def800a
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-04-20 21:32:50 -0400

    Added Styling to Header Component

--- assets/js/realEstate/Header.js
+++ assets/js/realEstate/Header.js
@@ -11,7 +11,7 @@ export default class Header extends Component {
   render () {
     return (
       <header>
-        <div> Logo </div>
+        <div className="logo"> Logo </div>
 
         <nav>
         <a href="#"> Create Ads </a>
