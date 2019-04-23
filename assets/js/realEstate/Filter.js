b93aab273c957eec1015a618244df2a2c665239f
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-04-22 20:11:09 -0400

    Adding Styles to Filter section and HTML for Listings

--- assets/js/realEstate/Filter.js
+++ assets/js/realEstate/Filter.js
@@ -11,43 +11,50 @@ export default class Filter extends Component {
   render () {
     return (
       <section id="filter">
-        <h4> Filter </h4>
-        <select name="neighbourhood" className="filters neighbourhood">
-          <option> Ridgewood </option>
-        </select>
-        <select name="houseType" className="filters housetype">
-          <option> Ranch </option>
-        </select>        
-        <select name="bedrooms" className="filters bedrooms">
-          <option> 2 BR </option>
-        </select>
-        <div className="filters price">
-          <span className="title"> Price </span>
-          <input type="text" name="min-price" className="min-price"/>
-          <input type="text" name="max-price" className="max-price"/>
-        </div>        
-        <div className="filters floor-space">
-          <span className="title"> Floor space </span>
-          <input type="text" name="min-floor-space" className="min-floor-space"/>
-          <input type="text" name="max-floor-space" className="max-floor-space"/>
-        </div>
-        <div className="filters extras">
-        <label for="extras">
-          <span> Elevators </span> 
-          <input type="checkbox" value="elevator" name="extras"/>
-        </label>
-        <label for="extras">
-          <span> Swimming Pool </span> 
-          <input type="checkbox" value="swimming-pool" name="extras"/>
-        </label>
-        <label for="extras">
-          <span> Finished Basement </span> 
-          <input type="checkbox" value="finished-basement" name="extras"/>
-        </label>
-        <label for="extras">
-          <span> Gym </span> 
-          <input type="checkbox" value="gym" name="extras"/>
-        </label>
+        <div className="inside">
+          <h4> Filter </h4>
+          
+          <select name="neighbourhood" className="filters neighbourhood">
+            <option> Ridgewood </option>
+          </select>
+          <select name="houseType" className="filters housetype">
+            <option> Ranch </option>
+          </select>        
+          <select name="bedrooms" className="filters bedrooms">
+            <option> 2 BR </option>
+          </select>
+
+          <div className="filters price">
+            <span className="title"> Price </span>
+            <input type="text" name="min-price" className="min-price"/>
+            <input type="text" name="max-price" className="max-price"/>
+          </div>        
+          
+          <div className="filters floor-space">
+            <span className="title"> Floor space </span>
+            <input type="text" name="min-floor-space" className="min-floor-space"/>
+            <input type="text" name="max-floor-space" className="max-floor-space"/>
+          </div>
+          
+          <div className="filters extras">
+          <span className="title"> Extras</span>
+          <label for="extras">
+            <span> Elevators </span> 
+            <input type="checkbox" value="elevator" name="extras"/>
+          </label>
+          <label for="extras">
+            <span> Swimming Pool </span> 
+            <input type="checkbox" value="swimming-pool" name="extras"/>
+          </label>
+          <label for="extras">
+            <span> Finished Basement </span> 
+            <input type="checkbox" value="finished-basement" name="extras"/>
+          </label>
+          <label for="extras">
+            <span> Gym </span> 
+            <input type="checkbox" value="gym" name="extras"/>
+          </label>
+          </div>
         </div>
       </section>
     )
