c0f65a987a9120576300be2addb2fcc64af8f957
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-04-24 09:22:49 -0400

    Small JSX Update

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -30,41 +30,127 @@ export default class Listings extends Component {
           </section>
 
           <section className="listings-results">
-            <div className="listing">
-              <div className="listing-img">
-                <span className="Address"> Address </span>
-                <div className="details">
-                  <div className="user-img"></div>
-                  <div className="user-details">
-                    <span className="user-name"> Nina Smith </span>
-                    <span className="post-date"> 05/05/2017</span>
-                  </div>
-                  <div className="listing-details">
-                    <div className="floor-space"><i className="fa fa-square-o" aria-hidden="true"></i> <span>1000ft&sup2;</span></div>
-                    <div className="bedrooms">
-                      <i className="fa fa-bed" aria-hidden="true"></i>
-                      <span>3 bedrooms</span>
+            <div className="col-md-3">
+              <div className="listing">
+                <div className="listing-img">
+                  <span className="address"> 420 High Wood Falls </span>
+                  <div className="details">
+                    <div className="col-md-3">
+                      <div className="user-img"></div>
+                    </div>
+                    <div className="col-md-9">
+                      <div className="user-details">
+                        <span className="user-name"> Nina Smith </span>
+                        <span className="post-date"> 05/05/2017</span>
+                      </div>
+                      <div className="listing-details">
+                        <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true"></i> <span>1000ft&sup2;</span>
+                        </div>
+                        <div className="bedrooms">
+                          <i className="fa fa-bed" aria-hidden="true"></i>
+                          <span>3 bedrooms</span>
+                        </div>
+                      </div>
+
+                      <div className="view-btn">
+                        View Listing
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="bottom-info">
-                <span> $1000 / month </span>
-                <span> <i className="fa fa-map-marker" aria-hidden="true"></i> RidgeWood, NY </span>
+                <div className="bottom-info">
+                  <span className="price"> $1000 / month </span>
+                  <span className="location"> <i className="fa fa-map-marker" aria-hidden="true"></i> RidgeWood, NY </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div className="listing">
+                <div className="listing-img">
+                  <span className="address"> 420 High Wood Falls </span>
+                  <div className="details">
+                    <div className="col-md-3">
+                      <div className="user-img"></div>
+                    </div>
+                    <div className="col-md-9">
+                      <div className="user-details">
+                        <span className="user-name"> Nina Smith </span>
+                        <span className="post-date"> 05/05/2017</span>
+                      </div>
+                      <div className="listing-details">
+                        <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true"></i> <span>1000ft&sup2;</span>
+                        </div>
+                        <div className="bedrooms">
+                          <i className="fa fa-bed" aria-hidden="true"></i>
+                          <span>3 bedrooms</span>
+                        </div>
+                      </div>
+
+                      <div className="view-btn">
+                        View Listing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="bottom-info">
+                  <span className="price"> $1000 / month </span>
+                  <span className="location"> <i className="fa fa-map-marker" aria-hidden="true"></i> RidgeWood, NY </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-3">
+              <div className="listing">
+                <div className="listing-img">
+                  <span className="address"> 420 High Wood Falls </span>
+                  <div className="details">
+                    <div className="col-md-3">
+                      <div className="user-img"></div>
+                    </div>
+                    <div className="col-md-9">
+                      <div className="user-details">
+                        <span className="user-name"> Nina Smith </span>
+                        <span className="post-date"> 05/05/2017</span>
+                      </div>
+                      <div className="listing-details">
+                        <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true"></i> <span>1000ft&sup2;</span>
+                        </div>
+                        <div className="bedrooms">
+                          <i className="fa fa-bed" aria-hidden="true"></i>
+                          <span>3 bedrooms</span>
+                        </div>
+                      </div>
+
+                      <div className="view-btn">
+                        View Listing
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="bottom-info">
+                  <span className="price"> $1000 / month </span>
+                  <span className="location"> <i className="fa fa-map-marker" aria-hidden="true"></i> RidgeWood, NY </span>
+                </div>
               </div>
             </div>
           </section>
 
-          <section className="pagination">
-            <ul className="pagination-nums">
-            <li>Prev</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>Next</li>
+          <section id="pagination">
+            <ul className="pages">
+              <li>Prev</li>
+              <li className="active">1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+              <li>Next</li>
             </ul>
           </section>
 
