364e4104fe258ec2689c94641dd1ae899e940eb9
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-04-24 19:54:58 -0400

    Adding dummy data to our app for prototyping purposes

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,74 @@
+var listingsData = [
+  {
+    address: '20-34 grand ave',
+    city: 'Ridgewood',
+    state: 'NY',
+    rooms: 3,
+    price: 220000,
+    floorSpacE: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType: 'Apartment',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb44VAzO907fDCghYaRdW55LnX40bzhMH7JL5c_fog_mWLJwFmeQ'
+  },
+  {
+    address: '2000 grantimo blvd',
+    city: 'San Franciso',
+    state: 'CA',
+    rooms: 4,
+    price: 5000000,
+    floorSpacE: 10000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType: 'Apartment',
+    image: 'https://s-ec.bstatic.com/images/hotel/max1024x768/735/73580604.jpg'
+  },
+  {
+    address: '1700 young street',
+    city: 'Toronto',
+    state: 'ON',
+    rooms: 4,
+    price: 2400000,
+    floorSpacE: 6000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType: 'Apartment',
+    image: 'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_1100,w_2000/v1504090383/g5/g5-c-i7yxybw5-mission-rock-single/g5-cl-iap27qrg-mountain-view-apartment-homes/uploads/interior-4.jpg'
+  },
+  {
+    address: '500 booly ave',
+    city: 'Tampa Bay',
+    state: 'Florida',
+    rooms: 2,
+    price: 2200000,
+    floorSpacE: 5000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType: 'Apartment',
+    image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+  },
+  {
+    address: '45 hockey ave',
+    city: 'Vancouver',
+    state: 'BC',
+    rooms: 2,
+    price: 100000,
+    floorSpacE: 4000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType: 'Apartment',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDELw8AmhnKoNtnlmfe1yIH-Ndkw-8dRzEtumfzcqtL_PXZvf0dQ'
+  }
+]
+
+export default listingsData;
