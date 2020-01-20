12966d33f39601d6c055b555191914f4851616cc
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-01-20 10:37:52 -0500

    Adding webpack file and start script to package.json

---
+++ v1/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([{ from: 'public/index.html' }]),
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, './public'),
+    hot: true,
+    open: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
