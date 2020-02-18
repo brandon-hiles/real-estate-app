323257b5c9cf5bc5de59374337af7f73b2a16dc1
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-02-17 20:11:43 -0500

    updating data

--- v1/src/components/data/listingsData.js
+++ v1/src/components/data/listingsData.js
@@ -1,84 +1,469 @@
 var listingsData = [
-    {
-      address: '20-34 grand ave',
-      city: 'Ridgewood',
-      state: 'NY',
-      rooms: 3,
-      price: 220000,
-      floorSpace: 2000,
-      extras: {
-        elevator: true,
-        gym: true,
-        finished_basement: false,
-        pool: false
-      },
-      homeType: 'Apartment',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb44VAzO907fDCghYaRdW55LnX40bzhMH7JL5c_fog_mWLJwFmeQ'
+  {
+    "address": "838 Heather Dr.",
+    "city": "Wheeling",
+    "state": "WV",
+    "rooms": "3",
+    "price": 333020,
+    "floorSpace": 11730,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": true,
+        "pool": false
     },
-    {
-      address: '2000 grantimo blvd',
-      city: 'San Franciso',
-      state: 'CA',
-      rooms: 4,
-      price: 5000000,
-      floorSpace: 10000,
-      extras: {
-        elevator: true,
-        gym: false,
-        finished_basement: false,
-        pool: true
-      },
-      homeType: 'Studio',
-      image: 'https://s-ec.bstatic.com/images/hotel/max1024x768/735/73580604.jpg'
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_1.jpg"
+},
+{
+    "address": "153 Canal Street",
+    "city": "Neptune",
+    "state": "CT",
+    "rooms": 3,
+    "price": 410707,
+    "floorSpace": 11528,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": true,
+        "pool": false
     },
-    {
-      address: '1700 young street',
-      city: 'Toronto',
-      state: 'ON',
-      rooms: 4,
-      price: 2400000,
-      floorSpace: 6000,
-      extras: {
-        elevator: false,
-        gym: true,
-        finished_basement: true,
-        pool: true
-      },
-      homeType: 'Ranch',
-      image: 'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_1100,w_2000/v1504090383/g5/g5-c-i7yxybw5-mission-rock-single/g5-cl-iap27qrg-mountain-view-apartment-homes/uploads/interior-4.jpg'
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_2.jpg"
+},
+{
+    "address": "246 Hamilton Drive",
+    "city": "Dallas",
+    "state": "GA",
+    "rooms": 2,
+    "price": 322365,
+    "floorSpace": 11169,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": false
     },
-    {
-      address: '500 booly ave',
-      city: 'Tampa Bay',
-      state: 'Florida',
-      rooms: 2,
-      price: 2200000,
-      floorSpace: 5000,
-      extras: {
-        elevator: false,
-        gym: true,
-        finished_basement: false,
-        pool: true
-      },
-      homeType: 'Studio',
-      image: 'https://07f138315bb5e97f9e43-31068357019044cca7c8e84d92de0d99.ssl.cf3.rackcdn.com/1024x768/56587_11491_001.jpg'
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_3.jpg"
+},
+{
+    "address": "122 Brown Street",
+    "city": "Onalaska",
+    "state": "WI",
+    "rooms": 5,
+    "price": 689407,
+    "floorSpace": 7754,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": false,
+        "pool": false
     },
-    {
-      address: '45 hockey ave',
-      city: 'Vancouver',
-      state: 'BC',
-      rooms: 2,
-      price: 100000,
-      floorSpace: 4000,
-      extras: {
-        elevator: true,
-        gym: true,
-        finished_basement: true,
-        pool: true
-      },
-      homeType: 'Apartment',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDELw8AmhnKoNtnlmfe1yIH-Ndkw-8dRzEtumfzcqtL_PXZvf0dQ'
-    }
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_4.jpg"
+},
+{
+    
+    "address": "903 Clay Avenue",
+    "city": "Quakertown",
+    "state": "PA",
+    "rooms": 2,
+    "price": 201440,
+    "floorSpace": 16322,
+    "extras": {
+        "elecator": false,
+        "gym": false,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_5.jpg"
+},
+{
+    "address": "7310 Trenton Rd.",
+    "city": "Plattsburgh",
+    "state": "NY",
+    "rooms": 2,
+    "price": 54230,
+    "floorSpace": 3725,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": false
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_6.jpg"
+},
+{
+    "address": "82 New St.",
+    "city": "Littleton",
+    "state": "CO",
+    "rooms": 4,
+    "price": 393977,
+    "floorSpace": 5870,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_7.jpg"
+},
+{
+    "address": "802 elm st.",
+    "city": "Clementon",
+    "state": "NJ",
+    "rooms": 3,
+    "price": 816238,
+    "floorSpace": 17004,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": false
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_8.jpg"
+},
+{
+    "address": "3 Beach Court",
+    "city": "Liverpool",
+    "state": "NY",
+    "rooms": 1,
+    "price": 439828,
+    "floorSpace": 16349,
+    "extras": {
+        "elecator": false,
+        "gym": false,
+        "finished_basement": true,
+        "pool": false
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_9.jpg"
+},
+{
+    "address": "38 Griffin St.",
+    "city": "Detroit",
+    "state": "MI",
+    "rooms": 3,
+    "price": 416736,
+    "floorSpace": 11085,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_10.jpg"
+},
+{
+    "address": "9 Thorne Drive",
+    "city": "Middleburg",
+    "state": "FL",
+    "rooms": 4,
+    "price": 708904,
+    "floorSpace": 12728,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_11.jpg"
+},
+{
+    "address": "332 Winchester Ave",
+    "city": "Antioch",
+    "state": "TN",
+    "rooms": 3,
+    "price": 675091,
+    "floorSpace": 8361,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": true,
+        "pool": false
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_12.jpg"
+},
+{
+    "address": "745 Rockville Ave.",
+    "city": "Avon",
+    "state": "IN",
+    "rooms": 3,
+    "price": 196761,
+    "floorSpace": 2143,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": false
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_13.jpg"
+},
+{
+    "address": "941 Lancaster St.",
+    "city": "Lake Villa",
+    "state": "IL",
+    "rooms": 5,
+    "price": 740595,
+    "floorSpace": 16487,
+    "extras": {
+        "elecator": false,
+        "gym": false,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_14.jpg"
+},
+{
+    "address": "2 Gregory Ave.",
+    "city": "Noblesville",
+    "state": "IN",
+    "rooms": 3,
+    "price": 559728,
+    "floorSpace": 8580,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_15.jpg"
+},
+{
+    "address": "564 Airport Street",
+    "city": "Ashburn",
+    "state": "VA",
+    "rooms": 3,
+    "price": 326658,
+    "floorSpace": 15002,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_16.jpg"
+},
+{
+    "address": "436 Golf Street",
+    "city": "Hampton",
+    "state": "VA",
+    "rooms": 6,
+    "price": 537433,
+    "floorSpace": 17845,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_17.jpg"
+},
+{
+    "address": "301 Brown Ave.",
+    "city": "Waterloo",
+    "state": "IA",
+    "rooms": 5,
+    "price": 836235,
+    "floorSpace": 15330,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": true,
+        "pool": false
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_18.jpg"
+},
+{
+    "address": "88 Del Monte Lane",
+    "city": "Baldwin",
+    "state": "NY",
+    "rooms": 2,
+    "price": 64002,
+    "floorSpace": 9944,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": true,
+        "pool": false
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_19.jpg"
+},
+{
+    "address": "4 Ryan St.",
+    "city": "Sykesville",
+    "state": "MD",
+    "rooms": 5,
+    "price": 486087,
+    "floorSpace": 11014,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_20.jpg"
+},
+{
+    "address": "406 Princess Drive",
+    "city": "Gettysburg",
+    "state": "PA",
+    "rooms": 6,
+    "price": 647098,
+    "floorSpace": 15312,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_21.jpg"
+},
+{
+    "address": "903 Wild Rose Drive",
+    "city": "Long Branch",
+    "state": "NJ",
+    "rooms": 5,
+    "price": 395804,
+    "floorSpace": 15418,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_22.jpg"
+},
+{
+    "address": "545 Kent Ave.",
+    "city": "Ridgewood",
+    "state": "NJ",
+    "rooms": 6,
+    "price": 334998,
+    "floorSpace": 5503,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_23.jpg"
+},
+{
+    "address": "33 Deport Rd.",
+    "city": "Circle Pines",
+    "state": "MN",
+    "rooms": 3,
+    "price": 736163,
+    "floorSpace": 18198,
+    "extras": {
+        "elecator": true,
+        "gym": false,
+        "finished_basement": true,
+        "pool": false
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_24.jpg"
+},
+{
+    "address": "542 Warren St.",
+    "city": "Frankfort",
+    "state": "KY",
+    "rooms": 4,
+    "price": 958064,
+    "floorSpace": 6960,
+    "extras": {
+        "elecator": true,
+        "gym": true,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Cottage",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_25.jpg"
+},
+{
+    "address": "2380 Glendale Drive",
+    "city": "Anderson",
+    "state": "SC",
+    "rooms": 2,
+    "price": 341604,
+    "floorSpace": 12998,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": true,
+        "pool": true
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_26.jpg"
+},
+{
+    "address": "482 Old York Rd.",
+    "city": "Douglasville",
+    "state": "GA",
+    "rooms": 3,
+    "price": 927364,
+    "floorSpace": 14644,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Ranch",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_27.jpg"
+},
+{
+    "address": "164 Roberts Rd.",
+    "city": "Bergenfield",
+    "state": "NJ",
+    "rooms": 2,
+    "price": 619123,
+    "floorSpace": 8923,
+    "extras": {
+        "elecator": false,
+        "gym": false,
+        "finished_basement": false,
+        "pool": true
+    },
+    "homeType": "Apartment",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_28.jpg"
+},
+{
+    "address": "580 Country Street",
+    "city": "Beltsville",
+    "state": "MD",
+    "rooms": 2,
+    "price": 799726,
+    "floorSpace": 15929,
+    "extras": {
+        "elecator": false,
+        "gym": true,
+        "finished_basement": false,
+        "pool": false
+    },
+    "homeType": "Studio",
+    "image": "https://cedar-property-advisors.s3.ca-central-1.amazonaws.com/img_29.jpg"
+}
   ]
   
   export default listingsData;
