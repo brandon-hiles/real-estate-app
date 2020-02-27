6a0fb3e1b7b105a28048d3134c45b74fd55a97a2
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-02-27 18:41:33 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,10 @@ const port = 3000
 // Routes
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/upload', (req, res) => res.send(aws.upload('./controllers/data/listingsData.json')))
-app.get('/data', (req, res) => res.send(aws.grabData()))
+app.get('/data', (req, res) => {
+    let data = aws.grabData('Listings')
+    let results = data.then(result => JSON.stringify(result))
+    res.send(results)
+})
 
-app.get('/store_data', (req, res) => res.send(Window.localStorage()))
 app.listen(port, () => console.log("Server has started"))
