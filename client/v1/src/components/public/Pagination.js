eb9297e4c5ce5b60dac63936a3b59a57fe8003a0
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-05-15 19:42:55 -0400

    Partition data function is complete

--- client/v1/src/components/public/Pagination.js
+++ client/v1/src/components/public/Pagination.js
@@ -3,24 +3,28 @@ import React, { Component } from 'react'
 const axios = require('axios');
 
 export default class Pagination extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             prev: false,
             next: true,
             current: "1",
+            listingsData: []
         }
         this.paritionData = this.paritionData.bind(this)
     }
 
     componentDidMount() {
         const NUMBER = '24'; // Number of files we want to test
-        const URI = 'https://stupefied-mccarthy-ecaf46.netlify.com/.netlify/functions/api/data/'; // API Endpoint
+        const URI = 'https://stupefied-mccarthy-ecaf46.netlify.app/.netlify/functions/api/data/'; // API Endpoint
 
-        const HOME_URL = URI + 'housing/' + NUMBER;
+        //const HOME_URL = URI + 'housing/' + NUMBER;
+        const HOME_URL = URI + 'housing';
         axios.get(HOME_URL).then(res => {
+            console.log(res.data)  
+            let data = this.paritionData(res.data)
             this.setState({
-              listingsData: res.data
+              listingsData: data
             })
           })
     }
@@ -29,9 +33,14 @@ export default class Pagination extends Component {
         /*
         Problem: Accessing listingsData variable in the state.
         */
-        let filter = []
-        let indexArr = [...Array(data.length/12).keys().mas(x =>++x)] // Generate an array of 1 to N/12
-        console.log(indexArr)
+        let filterData = []
+        let indexArr = [...Array(Math.round(data.length/12)).keys()].map(x => ++x) // Generate an array of 1 to N/12
+
+      for (let i = 0; i < indexArr.length; i++) {
+        filterData.push(data.slice(i*12,i*12+12))
+      }
+
+        return filterData
     }
 
     render() {
@@ -40,13 +49,13 @@ export default class Pagination extends Component {
               <div className="row">
                 <ul className="pages">
                   <li>Prev</li>
+                  {/* Auto-generate these numbers */}
                   <li className="active">1</li>
                   <li>2</li>
                   <li>3</li>
                   <li>4</li>
                   <li>5</li>
                   <li>Next</li>
-                  {this.paritionData(this.state.listingsData)}
                 </ul>
               </div>
             </section>
