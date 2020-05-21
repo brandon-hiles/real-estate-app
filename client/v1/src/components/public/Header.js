8b5d4103c43e979bb1790ba478e0acc42ab03670
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-05-21 19:31:57 -0400

    Cleaning up Header component

--- client/v1/src/components/public/Header.js
+++ client/v1/src/components/public/Header.js
@@ -1,30 +1,21 @@
 import React, { Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import '../../sass/app.scss';
 
 export default class Header extends Component {
-  constructor () {
-    super()
-    this.state = {
-      name: 'Brandon',
-    }
-  }
 
   render () {
     return (
-      <Router>
         <header id="loggedout_header">
           <div className="logo"> Cedar Property Adivsors </div>
 
           <nav>
-            <Link to='/get-started'> Create Ads </Link>
-            <Link to='/about-us'> About Us </Link>
-            <Link to='/login'> Login </Link>
-            <Link to='/register' className="register-btn"> Register </Link>
+            <a href="#"> Create Ads </a>
+            <a href="#"> About Us </a>
+            <a href="#"> Login </a>
+            <a href="#" className="register-btn"> Register </a>
           </nav>
       </header>
-      </Router>
     )
   }
 }
