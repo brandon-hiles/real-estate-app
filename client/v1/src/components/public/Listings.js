5cafcffab12e3c277d12f6a956586bf1b6481fdc
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-05-27 08:10:09 -0400

    Update Listings and Pagination

--- client/v1/src/components/public/Listings.js
+++ client/v1/src/components/public/Listings.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 
 // View Components
-import BoxView from '../views/BoxView'
-import LongView from '../views/LongView'
+import BoxView from '../views/boxview'
+import LongView from '../views/longview'
+import Pagination from './pagination'
 
 // Styles
 import '../../sass/app.scss';
@@ -29,7 +30,7 @@ export default class Listings extends Component {
         Generate our listings data
       */
 
-        var {listingsData} = this.props
+        var {listingsData, users} = this.props
 
         if(listingsData == undefined || listingsData.length == 0) {
             return "Sorry your filter did not match any listing";
@@ -37,11 +38,11 @@ export default class Listings extends Component {
       
           if(this.props.globalState.view == 'box') { // Box view
             return (
-              <BoxView listingsData = {listingsData}/>
+              <BoxView listingsData = {listingsData} users = {users}/>
             )
           } else { // Long view
             return (
-              <LongView listingsData = {listingsData} />
+              <LongView listingsData = {listingsData} users = {users}/>
               )
           }
     }
@@ -71,67 +72,8 @@ export default class Listings extends Component {
               {this.loopListings()}
             </section>
   
-            <section id="pagination">
-              <div className="row">
-                <ul className="pages">
-                  <li>Prev</li>
-                  <li className="active">1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                  <li>5</li>
-                  <li>Next</li>
-                </ul>
-              </div>
-            </section>
-  
-  
-          </section>
-      )
-    }    
-/*
-    render () {
-      return (
-          <section id="listings">
-            <section className="search-area">
-              <input type="text" name="search" onChange={this.props.change}/>
-            </section>
-  
-            <section className="sortby-area">
-              <div className="results"> {this.props.globalState.filteredData.length} results found </div>
-              <div className="sort-options">
-                <select name="sortby" className="sortby" onChange={this.props.change}>
-                  <option value="price-dsc"> Lowest Price </option>
-                  <option value="price-asc"> Highest Price </option>
-                </select>
-                <div className="view">
-                  <i className="fa fa-th-list" aria-hidden="true" onClick={this.props.changeView.bind(null, "long")}></i>
-                  <i className="fa fa-th" aria-hidden="true" onClick={this.props.changeView.bind(null, "box")}></i>
-                  </div>
-              </div>
-            </section>
-  
-            <section className="listings-results">
-              {this.loopListings()}
-            </section>
-  
-            <section id="pagination">
-              <div className="row">
-                <ul className="pages">
-                  <li>Prev</li>
-                  <li className="active">1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                  <li>5</li>
-                  <li>Next</li>
-                </ul>
-              </div>
-            </section>
-  
-  
+            <Pagination />
           </section>
       )
     }
-    */    
 }
--- client/v1/src/components/public/Pagination.js
+++ client/v1/src/components/public/Pagination.js
@@ -5,43 +5,11 @@ const axios = require('axios');
 export default class Pagination extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            listingsData: []
-        }
-        this.paritionData = this.paritionData.bind(this)
-    }
-
-    componentDidMount() {
-        const NUMBER = '24'; // Number of files we want to test
-        const URI = 'https://stupefied-mccarthy-ecaf46.netlify.app/.netlify/functions/api/data/'; // API Endpoint
-
-        //const HOME_URL = URI + 'housing/' + NUMBER;
-        const HOME_URL = URI + 'housing';
-        axios.get(HOME_URL).then(res => {
-            console.log(res.data)  
-            let data = this.paritionData(res.data)
-            this.setState({
-              listingsData: data
-            })
-          })
-    }
-
-    paritionData(data) {
-        /*
-        Problem: Accessing listingsData variable in the state.
-        */
-        let filterData = []
-        let indexArr = [...Array(Math.round(data.length/12)).keys()].map(x => ++x) // Generate an array of 1 to N/12
-
-      for (let i = 0; i < indexArr.length; i++) {
-        filterData.push(data.slice(i*12,i*12+12))
-      }
-
-        return filterData
     }
 
     render() {
-      let arr = [...Array(this.state.listingsData.length).keys()].map(x => ++x)
+      let 
+      //let arr = [...Array(this.state.listingsData.length).keys()].map(x => ++x)
         return(
             <section id="pagination">
               <div className="row">
