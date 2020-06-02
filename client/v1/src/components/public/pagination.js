687165ef2ae4531d55f6166be71c38851eb2ff69
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-06-01 22:06:41 -0400

    Updating pagination

--- client/v1/src/components/public/pagination.js
+++ client/v1/src/components/public/pagination.js
@@ -8,15 +8,15 @@ export default class Pagination extends Component {
     }
 
     render() {
-      let 
-      //let arr = [...Array(this.state.listingsData.length).keys()].map(x => ++x)
-        return(
+      var {data, current} = this.props
+      let arr = [...Array(data.length).keys()].map(x => ++x)
+      return(
             <section id="pagination">
               <div className="row">
                 <ul className="pages">
                   <li id="prev">Prev</li>
                   {arr.map((val) => {
-                    if ({val} == this.state.current) {
+                    if ({val} == current) {
                       return (<li key={val} className="active"> {val} </li>)
                     } else {
                       return(<li key={val} id= {val}> {val} </li>)
