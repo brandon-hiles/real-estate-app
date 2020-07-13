4b6f296c0f1d160799cc2bee905f3295fadacb29
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-07-13 11:24:26 -0400

    Update index.js

--- client/v1/src/index.js
+++ client/v1/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import Pagination from './components/public/Pagination';
 
-//ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<Pagination />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
